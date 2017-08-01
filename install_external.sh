3dacf6741f12aea6f998635d338fb81ba798677a
Gyorgy Orosz <gyorgy.orosz@logmein.com>
2017-08-01 23:24:22 +0200

    Refactor

--- install_external.sh
+++ install_external.sh
@@ -2,5 +2,9 @@
 
 pip install https://github.com/oroszgy/hunlp/releases/download/0.2/hunlp-0.2.0.tar.gz
 
+#polyglot download TASK:ner2
+#polyglot download TASK:embeddings2
+
+
 docker pull oroszgy/hunlp
 docker pull dbpedia/spotlight-hungarian
