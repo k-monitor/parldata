6eae6a5df57d83574f00ae14b6512bbac8d0ed30
Zoltan Varju <zvarju@precognox.com>
2018-03-19 21:11:10 +0100

    add lda

--- src/topicmodel/src/lda_from_corpus.py
+++ src/topicmodel/src/lda_from_corpus.py
@@ -4,26 +4,19 @@ from datetime import datetime
 from gensim import corpora
 from gensim.models import LdaModel
 
-# uncomment logging, if you don't like it
+# logger, timer
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 print('Start')
 print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
-# open corpus
+# load corpus
 id2word = corpora.Dictionary.load('data/corpus/parla.dict')
 corpus = corpora.MmCorpus('data/corpus/parla.mm')
 
-stopwords = []
-# stoplist.txt
-f = codecs.open('data/corpus/stoplist.txt', 'r', 'utf-8')
-for l in f:
-    stopwords.append(l.strip())
-
 # train model
-n = 27
+n = 24
 lda = LdaModel(corpus, id2word=id2word, num_topics=n, alpha='auto',
-               eta='auto', iterations=300, passes=1, update_every=1,
-               chunksize=10000)
+               eta='auto', iterations=100, passes=1, update_every=1,
+               chunksize=50000)
 lda.save('data/corpus/parla.model')
-print('Done')
 print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
--- src/topicmodel/src/lda_viz.py
+++ src/topicmodel/src/lda_viz.py
@@ -3,7 +3,7 @@ import pyLDAvis.gensim
 
 dictionary = gensim.corpora.Dictionary.load('data/corpus/parla.dict')
 corpus = gensim.corpora.MmCorpus('data/corpus/parla.mm')
-lda = gensim.models.ldamodel.LdaModel.load('data/corpus/parla2.model')
+lda = gensim.models.ldamodel.LdaModel.load('data/corpus/parla.model')
 
 t = pyLDAvis.gensim.prepare(lda, corpus, dictionary)
 #pyLDAvis.show(t)
